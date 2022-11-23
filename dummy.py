34294cefe2ca727c6d8d3e69832c900fb7933612
Ambaks <78621642+Ambaks@users.noreply.github.com>
2022-11-23 14:38:47 -0600

    Added home class
    added home class

--- dummy.py
+++ dummy.py
@@ -1,3 +1,14 @@
-num = 3.5
+def func():
+    i = int(input('1 or 2\n'))
+    return i, func2(i)
 
+def func2(int):
+    if int == 1:
+        print('You passed')
+        return func()
+    else:
+        print('You lost')
+        
+        
 
+func()
--- test.py
+++ test.py
@@ -1,28 +1,61 @@
 import squat_module as sm
 from matplotlib import pyplot as plt
+import sys
 
-choice = int(input("Hello! Welcome back to your Nowva 0.One! What would you wish to do?\n1. Quick Exercise\n2. My Progress\n3. Start A Workout Plan\n4. Initialization\n"))
-if choice == 1:
-    choice = str(input('---------- Quick Exercise ----------\nChoose your exercise:\n - Squat\n - Bench Press\n - Deadlift\n'))
-    if choice == 'Squat':
-        reps = int(input('Input target repetition amount: '))
-        squat = sm.Squat(reps)
-        dataList = []
+class Home:
+  
+    def getChoice(choice):
+        if choice == 0:
+            choice = initialWelcome()
+            return choice
+        else:
+            choice = menu()
+        return choice
+    
+    def treatChoice(choice):
+        if choice == 1:
+            choice = str(input('---------- Quick Exercise ----------\nChoose your exercise:\n - Squat\n - Bench Press\n - Deadlift\n'))
+            if choice == 'Squat':
+                reps = int(input('Input target repetition amount: '))
+                squat = sm.Squat(reps)
+                dataList = []
 
-        dataList.append(squat.doSquat())
-        # print(dataList)
-        plt.show()
+                dataList.append(squat.doSquat())
+                # print(dataList)
+                plt.show()
+
+        if choice == 2:
+            pass
+
+        if choice == 3:
+            pass
+
+        if choice == 4:
+            pass
+
+        if choice == 5:
+            sys.exit()
+
+        return menu()
+        
 
 
+@staticmethod
+def initialWelcome():
+    choice = int(input("Hello! Welcome back to your Nowva 0.One! What would you wish to do?\n1. Quick Exercise\n2. My Progress\n3. Start A Workout Plan\n4. Initialization\n"))
+    return choice
+
+@staticmethod
+def menu():
+    choice = int(input("Main Menu:\n1. Quick Exercise\n2. My Progress\n3. Start A Workout Plan\n4. Initialization\n"))
+    return Home.treatChoice(choice)
 
 
+def main():
+    choice = 0
+    choice = Home.getChoice(choice)
+    Home.treatChoice(choice)
+    
 
-
-
-# Greeting
-
-# Pick exercise
-    #Add data to database
-
-# Ask for another exercise
-
+if __name__ == '__main__':
+    main()
