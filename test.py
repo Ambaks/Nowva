8506fc2254efaa8d5400e51f8a8a0564d3c1d6ec
Ambaks <78621642+Ambaks@users.noreply.github.com>
2022-11-23 12:10:00 -0600

    Added greeting
    greetings added

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import squat_module as sm
 from matplotlib import pyplot as plt
 
+choice = int(input("Hello! Welcome back to your Nowva 0.One! What would you wish to do?\n1. Quick Exercise\n2. My Progress\n3. Start A Workout Plan\n4. Initialization\n"))
+if choice == 1:
+    choice = str(input('---------- Quick Exercise ----------\nChoose your exercise:\n - Squat\n - Bench Press\n - Deadlift\n'))
+    if choice == 'Squat':
+        reps = int(input('Input target repetition amount: '))
+        squat = sm.Squat(reps)
+        dataList = []
 
-squat = sm.Squat()
-dataList = []
-
-dataList.append(squat.doSquat())
-# print(dataList)
-plt.show()
+        dataList.append(squat.doSquat())
+        # print(dataList)
+        plt.show()
 
 
 
